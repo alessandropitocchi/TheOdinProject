760527087a42851d263baf0e54f950746b3e8520
alessandropitocchi <110122679+alessandropitocchi@users.noreply.github.com>
2022-09-27 15:21:58 +0200

    Add files via upload

---
+++ JS/jsf-part-1/index.js
@@ -0,0 +1,20 @@
+const max = 57;
+
+let a = 10;
+let b = 7 * a;
+let actual = max - 13;
+let percentage = actual / max;
+
+console.log(23 + 97);
+
+console.log(20 + 10 + 32 + 36 + 78 + 90);
+
+console.log((4 + 6 + 9 ) / 77);
+
+console.log(a);
+
+console.log(9 * a);
+
+console.log(b);
+
+console.log("Percentage: " + percentage);
