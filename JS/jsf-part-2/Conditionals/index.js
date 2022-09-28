722700a2c05ed1a801aa1e7fd85044b09661d05f
Alessandro <alessandropitocchiap@gmail.com>
2022-09-28 22:02:42 +0200

    JS fundamentals part 2 - Conditionals - Switch

--- JS/jsf-part-2/Conditionals/index.js
+++ JS/jsf-part-2/Conditionals/index.js
@@ -59,3 +59,33 @@ if (hour < 10 || hour > 18 || isWeekend) {
   console.log(Message);
 
   console.log((hour == minute) ? 'Equal' : 'Not Equal');
+
+//switch
+
+const day = new Date().getDay();
+
+switch(day) {
+    case 0:
+        console.log('It\'s Sunday, time to relax!');
+        break;
+    case 1:
+        console.log('Happy Monday!');
+        break;
+    case 2:
+        console.log('It\'s Tuesday, You got this!');
+        break;
+    case 3:
+        console.log('Hump day already!');
+        break;
+    case 4:
+        console.log('Just one more day \'til the weekend!');
+        break;
+    case 5:
+        console.log('Happy Friday!');
+        break;
+    case 6:
+        console.log('Have a wonderful Saturday!');
+        break;
+    default:
+        console.log('Something went wrong!');
+}
