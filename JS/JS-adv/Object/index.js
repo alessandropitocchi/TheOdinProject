c6e5fb38e5a3773349e66e78c63841c87c7cc464
Alessandro Pitocchi <Alessandro.Pitocchi@elmec.it>
2022-10-17 15:31:14 +0200

    object constructor

---
+++ JS/JS-adv/Object/index.js
@@ -0,0 +1,12 @@
+function book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    }
+}
+
+const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read')
+console.log(theHobbit.info());
